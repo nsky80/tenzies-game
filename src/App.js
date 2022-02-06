96521703e6f53b265deff457b8d7c728e776348e
Satish NSKY80 <satishkumary80@gmail.com>
2022-02-07 00:52:58 +0530

    adding die component and styling it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
-
+import Die from './components/Die';
+/**
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
+ */
 function App() {
+
+
+
 	return (
 		<main>
-			Tenzie Game
+
+			<div className="dice-container">
+				<Die value="1" />
+				<Die value="2" />
+				<Die value="3" />
+				<Die value="4" />
+				<Die value="5" />
+				<Die value="6" />
+				<Die value="7" />
+				<Die value="8" />
+				<Die value="9" />
+				<Die value="10" />
+			</div>
+
 		</main>
 	);
 }
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+export default function Die(props){
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
