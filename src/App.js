41c436395b54d6541b26d96473162f93dc5f1197
Satish NSKY80 <satishkumary80@gmail.com>
2022-02-07 01:00:26 +0530

    random array added for dice

--- src/App.js
+++ src/App.js
@@ -3,36 +3,29 @@ import Die from './components/Die';
 /**
  * Challenge:
  * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
+ * Write a function (allNewDice) that returns an array 
+ * of 10 random numbers between 1-6 inclusive.
+ * 
+ * Log the array of numbers to the console for now
  */
 function App() {
 
+	function allNewDice(){
+		const randomDice = []
+		for(var i = 0; i < 10; i++){
+			randomDice.push(Math.floor((Math.random() * 6) + 1)
+			)
+		}
+		return randomDice
+	}
 
+	const dice = allNewDice().map(num => <Die value={num}/>)
 
 	return (
 		<main>
 
 			<div className="dice-container">
-				<Die value="1" />
-				<Die value="2" />
-				<Die value="3" />
-				<Die value="4" />
-				<Die value="5" />
-				<Die value="6" />
-				<Die value="7" />
-				<Die value="8" />
-				<Die value="9" />
-				<Die value="10" />
+				{dice}
 			</div>
 
 		</main>
